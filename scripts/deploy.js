3e1fda2e9c0f1ebdefafe97b9fb72ebd47f3b42f
7689000 <60218169+7689000@users.noreply.github.com>
2022-10-16 14:30:41 +0200

    Latest

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,14 +3,16 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+  
 
   // Call the function.
-  let txn = await nftContract.makeAnEpicNFT()
+  let txn = await nftContract.makeAnEpicNFT(receiverAddress, senderAddress)
   // Wait for it to be mined.
   await txn.wait()
   console.log("Minted NFT #1")
+  
 
-  txn = await nftContract.makeAnEpicNFT()
+  txn = await nftContract.makeAnEpicNFT(receiverAddress, senderAddress)
   // Wait for it to be mined.
   await txn.wait()
   console.log("Minted NFT #2")
